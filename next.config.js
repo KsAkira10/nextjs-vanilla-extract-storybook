950baff0cc7af98c7e9d4ba9d67cc74bfc6125ea
Lucas Akira Uehara <lucas.uehara@riachuelo.com.br>
2022-03-30 12:16:00 -0300

    chore: update configuration typescript

--- next.config.js
+++ next.config.js
@@ -4,6 +4,13 @@ const {
 const withVanillaExtract = createVanillaExtractPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {reactStrictMode: true,};
+const nextConfig = { 
+  reactStrictMode: true, 
+  webpack5: true,
+  images: {
+    loader: 'akamai',
+    path: 'http://localhost:3000',
+  },
+};
 
 module.exports = withVanillaExtract(nextConfig);
--- pages/storybook.tsx
+++ pages/storybook.tsx
@@ -1,4 +1,4 @@
-import { Page } from '../src/Page'
+import { Page } from '~@stories';
 
 export default function Storybook() {
     return (<Page />)
